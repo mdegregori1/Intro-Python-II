abc98cc450bbd7763b24824f274aff72a47379a8
Mauricio Degregori <degregori2@gmail.com>
2020-04-09 21:31:13 -0500

    fixed random variables

--- src/adv.py
+++ src/adv.py
@@ -14,25 +14,33 @@ items = [
 ]
 # package random import 
 # Declare all the rooms
-randomizer = random.choice(items)
+randomizer1 = random.choice(items)
+randomizer2 = random.choice(items)
+randomizer3 = random.choice(items)
+randomizer4 = random.choice(items)
+randomizer5 = random.choice(items)
+
+# for x in (items):
+#     global randomizer 
+#     randomizer = random.choice(items[x])
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", randomizer),
+                     "North of you, the cave mount beckons", randomizer1),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", randomizer),
+passages run north and east.""", randomizer2),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", randomizer),
+the distance, but there is no way across the chasm.""", randomizer3),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", randomizer),
+to north. The smell of gold permeates the air.""", randomizer4),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", randomizer),
+earlier adventurers. The only exit is to the south.""", randomizer5),
 }
 
 
